5425da4b537e827af8126856c4918f17a3da28c7
Stefan Probst <stefanprobst@posteo.de>
2024-02-09 20:13:36 +0100

    feat: add issue repro

---
+++ app.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+const { data } = await useAsyncData("content", () => {
+	return queryContent('/').findOne()
+})
+</script>
+
+<template>
+  <div>
+		<ContentRenderer v-if="data" :value="data" />
+  </div>
+</template>
